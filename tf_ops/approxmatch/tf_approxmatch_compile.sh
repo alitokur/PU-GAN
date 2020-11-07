7d814f47f23483500d942274b547fbe36eead817
alitokur <alitokurr@gmail.com>
2020-11-07 18:00:22 +0300

    ABI=1 to ABI=0

--- tf_ops/approxmatch/tf_approxmatch_compile.sh
+++ tf_ops/approxmatch/tf_approxmatch_compile.sh
@@ -3,4 +3,4 @@
 
 # TF1.4
 /usr/local/cuda-10.1/bin/nvcc tf_approxmatch_g.cu -o tf_approxmatch_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
-g++ -std=c++11 tf_approxmatch.cpp tf_approxmatch_g.cu.o -o tf_approxmatch_so.so -shared -fPIC -I /home/alitokur/.local/lib/python3.8/site-packages/tensorflow/include  -I /usr/local/cuda-10.1/include -I /home/alitokur/.local/lib/python3.8/site-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-10.1/lib64/ -L /home/alitokur/.local/lib/python3.8/site-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=1
+g++ -std=c++11 tf_approxmatch.cpp tf_approxmatch_g.cu.o -o tf_approxmatch_so.so -shared -fPIC -I /home/alitokur/.local/lib/python3.8/site-packages/tensorflow/include  -I /usr/local/cuda-10.1/include -I /home/alitokur/.local/lib/python3.8/site-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-10.1/lib64/ -L /home/alitokur/.local/lib/python3.8/site-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
